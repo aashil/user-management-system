9c5c8176b323a412fa700a3edd91a20ad3d0b3ad
aashil <aashilpatel1993@gmail.com>
2016-05-08 18:57:34 -0400

    first commit

---
+++ routes/users.js
@@ -0,0 +1,16 @@
+
+var express = require('express');
+var router = express.Router();
+
+/*
+ * GET userlist.
+ */
+router.get('/userlist', function(req, res) {
+    var db = req.db;
+    var collection = db.get('userlist');
+    collection.find({},{},function(e,docs){
+        res.json(docs);
+    });
+});
+
+module.exports = router;
